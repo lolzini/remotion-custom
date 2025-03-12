a682b374b1a71b334e5c771d5157bc4eef0607ea
lolzini <97997498+lolzini@users.noreply.github.com>
2025-03-12 11:49:01 -0600

    add: background composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import {AbsoluteFill, Folder} from 'remotion';
+import {AbsoluteFill, Composition, Folder} from 'remotion';
 import TikTokSlide from './components/tiktok-slide';
 import TikTokSlideLayout from './components/tiktok-slide-layout';
 
@@ -28,6 +28,11 @@ import Goals2025 from './stills/goals-2025';
 import DotBgOriginal from './compositions/dot-bg-original';
 import Thumbnail from './thumbnails/thumbnail';
 import Emoji from './emoji';
+
+import {useCurrentFrame, interpolate} from 'remotion';
+import Scales9 from './patterns/scales-9';
+import BrickWall1 from './patterns/brick-wall-1';
+import {z} from 'zod';
 
 const folders = [
 	{...convencionesDeNombramiento},
@@ -89,10 +94,55 @@ export const RemotionRoot: React.FC = () => {
 			<Folder name="thumbnails">
 				<Thumbnail />
 				<Emoji />
+				<Composition
+					id="background"
+					fps={FPS}
+					durationInFrames={DIF}
+					component={Background}
+					width={1920}
+					height={1080}
+					schema={schema}
+					defaultProps={{direction: 'ltr'}}
+				/>
 			</Folder>
 		</>
 	);
 };
+
+const FPS = 30;
+const DIF = FPS * 2;
+
+const schema = z.object({direction: z.enum(['rtl', 'ltr', 'btt', 'ttb'])});
+
+function Background({direction = 'btt'}: z.infer<typeof schema>) {
+	const frame = useCurrentFrame();
+
+	const getTransform = () => {
+		switch (direction) {
+			case 'ltr':
+				return `translateX(${interpolate(frame, [0, DIF], [-1920, 1920])}px)`;
+			case 'rtl':
+				return `translateX(${interpolate(frame, [0, DIF], [0, -1920])}px)`;
+			case 'ttb':
+				return `translateY(${interpolate(frame, [0, DIF], [-1080, 1080])}px)`;
+			case 'btt':
+				return `translateY(${interpolate(frame, [0, DIF], [0, -1080])}px)`;
+			default:
+				return `translateX(${interpolate(frame, [0, DIF], [0, -1920])}px)`;
+		}
+	};
+
+	return (
+		<AbsoluteFill style={{transform: getTransform()}}>
+			<div className="grid-col-2 grid">
+				<BrickWall1 width={1920} height={1080} />
+				<BrickWall1 width={1920} height={1080} />
+				<BrickWall1 width={1920} height={1080} />
+				<BrickWall1 width={1920} height={1080} />
+			</div>
+		</AbsoluteFill>
+	);
+}
 
 function ID0() {
 	return <></>;
---
+++ src/patterns/brick-wall-1.tsx
@@ -0,0 +1,33 @@
+export default function BrickWall1({
+	width = 1920,
+	height = 1080,
+	color = 'black',
+	background = '#ecc94b',
+}) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={width}
+			height={height}
+			viewBox={`0 0 ${width} ${height}`}
+		>
+			<defs>
+				<pattern
+					id="a"
+					width="30"
+					height="30"
+					patternTransform="scale(2)"
+					patternUnits="userSpaceOnUse"
+				>
+					<rect width="100%" height="100%" fill={background} />
+					<path
+						fill="none"
+						stroke={color}
+						d="M0 22.5h30v15H0zm15-15h30v15H15m-30-15h30v15h-30zm15-15h30v15H0z"
+					/>
+				</pattern>
+			</defs>
+			<rect width="800%" height="800%" fill="url(#a)" />
+		</svg>
+	);
+}
